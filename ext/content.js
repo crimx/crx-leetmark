27d16a9a6aa32a8a86f54ecf9c48c2f503daf79b
CRIMX <straybugs@gmail.com>
2018-07-12 14:05:50 +0800

    support ol and ul

--- ext/content.js
+++ ext/content.js
@@ -64,6 +64,28 @@ function extractProblem () {
     const href = ($el.getAttribute('href') || '').replace(/^\//, 'https://leetcode.com/')
     $el.outerHTML = `[${content}](${href})`
   })
+  
+  $problem.querySelectorAll('ul').forEach($ul => {
+    let content = ''
+    $ul.querySelectorAll(':scope > li').forEach($li => {
+      const liContent = ($li.textContent || '').trim()
+      if (liContent) {
+        content += `- ${liContent}\n`
+      }
+    })
+    $ul.outerHTML = content
+  })
+  
+  $problem.querySelectorAll('ol').forEach($ol => {
+    let content = ''
+    $ol.querySelectorAll(':scope > li').forEach(($li, i) => {
+      const liContent = ($li.textContent || '').trim()
+      if (liContent) {
+        content += `${i+1}. ${liContent}\n`
+      }
+    })
+    $ol.outerHTML = content
+  })
 
   $problem.querySelectorAll('p').forEach($el => {
     const content = ($el.textContent || '').trim()
